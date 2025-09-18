ff09644a6fa3b017a6d623496094942206e7bc7e
javierdp01 <100429736@alumnos.uc3m.es>
2025-09-18 20:14:02 +0200

    18-09
    Test l1b done

--- l1b/test/test_l1b.py
+++ l1b/test/test_l1b.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 from common.io.writeToa import writeToa, readToa
 from matplotlib import pyplot as plt
+
 
 # plot the isrf vs l1b
 
@@ -27,6 +30,67 @@ toa_l1b_1_myoutputsnoeq = readToa("C:\\Users\javie\Desktop\EODP\SHARED-20250911T
 toa_l1b_2_myoutputsnoeq = readToa("C:\\Users\javie\Desktop\EODP\SHARED-20250911T151756Z-1-001\SHARED\EODP_TER_2021\EODP-TS-L1B\myoutputs_noeq", "l1b_toa_VNIR-2.nc")
 toa_l1b_3_myoutputsnoeq = readToa("C:\\Users\javie\Desktop\EODP\SHARED-20250911T151756Z-1-001\SHARED\EODP_TER_2021\EODP-TS-L1B\myoutputs_noeq",  "l1b_toa_VNIR-3.nc")
 
+# We check the absolute error
+def check_band(toa_my, toa_ref, tol=0.01):
+    diff = np.abs(toa_ref - toa_my) / np.maximum(toa_ref, 1e-12) * 100
+    mu = np.mean(diff)
+    sigma = np.std(diff)
+
+    threshold = mu + 3*sigma
+
+    ok = threshold < tol
+    return mu, sigma, threshold, ok
+
+mu0, sigma0, threshold0, ok0 = check_band(toa_l1b_0_output, toa_l1b_0_myoutputs, tol=0.01)
+mu1, sigma1, threshold1, ok1 = check_band(toa_l1b_1_output, toa_l1b_1_myoutputs, tol=0.01)
+mu2, sigma2, threshold2, ok2 = check_band(toa_l1b_2_output, toa_l1b_2_myoutputs, tol=0.01)
+mu3, sigma3, threshold3, ok3 = check_band(toa_l1b_3_output, toa_l1b_3_myoutputs, tol=0.01)
+
+print("Band 0: mean=", mu0, " std=", sigma0, " threshold=", threshold0, " ok=", ok0)
+print("Band 1: mean=", mu1, " std=", sigma1, " threshold=", threshold1, " ok=", ok1)
+print("Band 2: mean=", mu2, " std=", sigma2, " threshold=", threshold2, "ok=", ok2)
+print("Band 3: mean=", mu3, " std=", sigma3, " threshold=", threshold3, "ok=", ok3)
+
+
 # We create the different plots
+plt.plot(  toa_isrf_0[0,:], label='isrf')
+plt.plot( toa_l1b_0_myoutputs[0,:], label='myoutputs')
+plt.title("isrf vs myoutputs 0")
+plt.legend()
+plt.show()
+plt.plot(  toa_isrf_1[0,:], label='isrf')
+plt.plot( toa_l1b_1_myoutputs[0,:], label='myoutputs')
+plt.title("isrf vs myoutputs 1")
+plt.legend()
+plt.show()
+plt.plot(  toa_isrf_2[0,:], label='myoutputs')
+plt.plot( toa_l1b_2_myoutputs[0,:], label='myoutputs')
+plt.title("isrf vs myoutputs 2")
+plt.legend()
+plt.show()
+plt.plot(  toa_isrf_3[0,:], label='isrf')
+plt.plot( toa_l1b_3_myoutputs[0,:], label='myoutputs')
+plt.title("isrf vs myoutputs 3")
+plt.legend()
+plt.show()
 
-
+plt.plot(  toa_l1b_0_myoutputsnoeq[0,:], label='myoutputsnoeq')
+plt.plot( toa_l1b_0_myoutputs[0,:], label='myoutputs')
+plt.title("myoutputs vs myoutputs_noeq 0")
+plt.legend()
+plt.show()
+plt.plot(  toa_l1b_1_myoutputsnoeq[0,:], label='myoutputsnoeq')
+plt.plot( toa_l1b_1_myoutputs[0,:], label='myoutputs')
+plt.title("myoutputs vs myoutputs_noeq 1")
+plt.legend()
+plt.show()
+plt.plot(  toa_l1b_2_myoutputsnoeq[0,:], label='myoutputsnoeq')
+plt.plot( toa_l1b_2_myoutputs[0,:], label='myoutputs')
+plt.title("myoutputs vs myoutputs_noeq 2")
+plt.legend()
+plt.show()
+plt.plot(  toa_l1b_3_myoutputsnoeq[0,:], label='myoutputsnoeq')
+plt.plot( toa_l1b_3_myoutputs[0,:], label='myoutputs')
+plt.title("myoutputs vs myoutputs_noeq 3")
+plt.legend()
+plt.show()
