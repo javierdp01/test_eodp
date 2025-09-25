a3884e32ef5ccef790b2f9820527afce9cfd30d5
javierdp01 <100429736@alumnos.uc3m.es>
2025-09-25 17:53:27 +0200

    25-09

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = r'C:\\Users\javie\Desktop\EODP\test_eodp\auxiliary'
+indir = r"C:\\Users\javie\Desktop\EODP\SHARED-20250911T151756Z-1-001\SHARED\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+outdir = r"C:\\Users\javie\Desktop\EODP\SHARED-20250911T151756Z-1-001\SHARED\EODP_TER_2021\EODP-TS-ISM\myoutputs"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
